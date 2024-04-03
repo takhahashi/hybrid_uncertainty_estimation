1e51feb95913c048bb26ac6381822a47b7fb3146
takhahashi <yuu1719m@ezweb.ne.jp>
2024-04-04 00:45:50 +0900

    Noe

--- src/run_tasks_on_multiple_gpus.py
+++ src/run_tasks_on_multiple_gpus.py
@@ -8,6 +8,7 @@ import logging
 from utils.utils_tasks import get_config
 
 import pdb
+import yaml
 
 log = logging.getLogger(__name__)
 
@@ -24,7 +25,11 @@ def run_task(task):
 
     command = f"CUDA_VISIBLE_DEVICES={utils.WORKER_CUDA_DEVICE} HYDRA_CONFIG_PATH={task.config_path} {task.environ} python {task.command} repeat={task.repeat} {task_args}"  # hydra.run.dir={run_dir} output_dir={output_dir}"
     #pdb.set_trace()
+    with open(task.config_path, 'r') as yml:
+        config = yaml.safe_load(yml)
+    print(config)
     log.info(f"Command: {command}")
+  
     
     ret = os.system(command)
     ret = str(ret)
