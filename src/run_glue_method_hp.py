75f77a661f6464eab4558e5790c957f109869c96
takhahashi <yuu1719m@ezweb.ne.jp>
2024-04-29 15:21:40 +0900

    None

--- src/run_glue_method_hp.py
+++ src/run_glue_method_hp.py
@@ -246,6 +246,7 @@ def do_predict_eval(
 
 
 def train_eval_glue_model(config, training_args, data_args, work_dir=None):
+    log.info(f"config:{config}")
     ue_args = config.ue
     model_args = config.model
 
