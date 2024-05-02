78dd235a897a90ccbbddcce5aa5d15d7a59bc701
takhahashi <yuu1719m@ezweb.ne.jp>
2024-05-02 13:38:11 +0900

    None

--- src/run_glue.py
+++ src/run_glue.py
@@ -635,7 +635,7 @@ def main(config):
     os.chdir(hydra.utils.get_original_cwd())
 
 
-    init_wandb(auto_generated_dir, config)
+    #init_wandb(auto_generated_dir, config)
 
     fix_config(config)
 
