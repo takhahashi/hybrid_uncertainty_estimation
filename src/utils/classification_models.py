cebf284ae2896887fd1dedd6a2b0e958671b4c13
takhahashi <yuu1719m@ezweb.ne.jp>
2024-05-29 20:58:52 +0900

    None

--- src/utils/classification_models.py
+++ src/utils/classification_models.py
@@ -1072,7 +1072,6 @@ class BertForSequenceRegression(BertPreTrainedModel):
         pooled_output = self.dropout(pooled_output)
         pred_score = self.sigmoid(self.score_predictor(pooled_output))
         pred_lnvar = self.variance_predictor(pooled_output)
-        print(f'batch_mean_lnvar:{torch.mean(pred_lnvar)}')
 
         loss = None
         if labels is not None:
