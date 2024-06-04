a05e4ffa7fae7f796659727492582c0098b87147
takhahashi <yuu1719m@ezweb.ne.jp>
2024-06-04 20:57:42 +0900

    None

--- src/train_gp.py
+++ src/train_gp.py
@@ -338,7 +338,7 @@ def train_eval_glue_model(config, training_args, data_args, work_dir=None):
 
         likelihood = gpytorch.likelihoods.GaussianLikelihood()
         GPmodel = GPModel(train_x, train_y, likelihood)
-        epoch = training_args.epoch
+        epoch = training_args.num_train_epochs
         GPmodel.train()
         likelihood.train()
         GPmodel.covar_module.base_kernel.lengthscale = np.linalg.norm(train_x[0].numpy() - train_x[1].numpy().T) ** 2 / 2
