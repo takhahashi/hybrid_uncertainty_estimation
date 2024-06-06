c9d49e1059df46ebd4ae7930a67513d9d87fd836
takhahashi <yuu1719m@ezweb.ne.jp>
2024-06-06 20:47:54 +0900

    None

--- src/run_tasks_for_model_series.py
+++ src/run_tasks_for_model_series.py
@@ -43,9 +43,6 @@ def run_glue_for_model_series_fast(config, work_dir):
         print(config.seeds)
         listdir = [int(seed) for seed in config.seeds]
     for model_dir_name in listdir:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(model_dir_name)
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!")
         if "fairlib" in str(config.model_series_dir) and "ensemble" not in str(
             config.model_series_dir
         ):
