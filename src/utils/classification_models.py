4fd612514898b786c13494e10fef6ea8e38adc51
takhahashi <yuu1719m@ezweb.ne.jp>
2024-06-08 20:17:29 +0900

    NOne

--- src/utils/classification_models.py
+++ src/utils/classification_models.py
@@ -931,6 +931,110 @@ class HybridBert(BertForSequenceClassification):
             distribution_label.append(norm_pdf_label)
         return torch.tensor(distribution_label).softmax(dim=1).cuda()
 
+class HybridDistilBert(DistilBertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.distilbert = DistilBertModel(config)
+        self.pre_classifier = nn.Linear(config.dim, config.dim)
+
+        self.classifier = nn.Linear(config.dim, config.num_labels)
+        self.regressor = nn.Linear(config.dim, 1)
+
+        self.dropout = nn.Dropout(config.seq_classif_dropout)
+        self.sigmoid = nn.Sigmoid()
+
+        self.lsb = ScaleDiffBalance(task_names=['regression', 'classification'])
+        self.scale_weights = {}
+        self.diff_weights = {}
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_position_embeddings(self) -> nn.Embedding:
+        """
+        Returns the position embeddings
+        """
+        return self.distilbert.get_position_embeddings()
+
+    def resize_position_embeddings(self, new_num_position_embeddings: int):
+        """
+        Resizes position embeddings of the model if `new_num_position_embeddings != config.max_position_embeddings`.
+
+        Arguments:
+            new_num_position_embeddings (`int`):
+                The number of new position embedding matrix. If position embeddings are learned, increasing the size
+                will add newly initialized vectors at the end, whereas reducing the size will remove vectors from the
+                end. If position embeddings are not learned (*e.g.* sinusoidal position embeddings), increasing the
+                size will add correct vectors at the end following the position encoding algorithm, whereas reducing
+                the size will remove vectors from the end.
+        """
+        self.distilbert.resize_position_embeddings(new_num_position_embeddings)
+
+    def forward(
+        self,
+        input_ids = None,
+        attention_mask = None,
+        head_mask = None,
+        inputs_embeds = None,
+        labels = None,
+        output_attentions = None,
+        output_hidden_states = None,
+        return_dict = None,
+    ):
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        distilbert_output = self.distilbert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        hidden_state = distilbert_output[0]  # (bs, seq_len, dim)
+        pooled_output = hidden_state[:, 0]  # (bs, dim)
+        pooled_output = self.pre_classifier(pooled_output)  # (bs, dim)
+        pooled_output = nn.ReLU()(pooled_output)  # (bs, dim)
+        pooled_output = self.dropout(pooled_output)  # (bs, dim)
+        
+        regressor_output = self.sigmoid(self.regressor(pooled_output))  # (bs, num_labels)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            ########regression loss########
+            reg_labels = labels.view(-1) / (self.num_labels - 1)
+            loss_fct = MSELoss()
+            r_loss = loss_fct(regressor_output.view(-1), reg_labels)  
+            
+            ########classification loss#########
+            loss_fct = CrossEntropyLoss()
+            c_loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            
+            loss, s_wei, diff_wei, alpha, pre_loss = self.lsb(regression=r_loss, classification=c_loss)
+            self.scale_weights = s_wei
+            self.diff_weights = diff_wei
+
+        if not return_dict:
+            output = (logits,) + distilbert_output[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return HybridOutput(
+            loss=loss,
+            logits=logits,
+            reg_output=regressor_output,
+            hidden_states=distilbert_output.hidden_states,
+            attentions=distilbert_output.attentions,
+        )
         
 class ScaleDiffBalance:
   def __init__(self, task_names, priority=None, beta=1.):
