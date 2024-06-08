dd48f32b95a836cc1f6dd6a5a7a1f9a1bcd12b2e
takhahashi <yuu1719m@ezweb.ne.jp>
2024-06-09 00:37:51 +0900

    None

--- src/run_glue.py
+++ src/run_glue.py
@@ -605,7 +605,7 @@ def train_eval_glue_model(config, training_args, data_args, work_dir):
             model,
             tokenizer,
             trainer,
-            test_dataset, ##############
+            eval_dataset, ##############
             train_dataset,
             calibration_dataset,
             metric,
