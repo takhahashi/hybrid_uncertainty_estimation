fda86cf8e0b386a11dc856ac12673d0ad47863b6
takhahashi <yuu1719m@ezweb.ne.jp>
2024-07-12 19:06:01 +0900

    Update run_tasks_for_model_series.py

--- src/run_tasks_for_model_series.py
+++ src/run_tasks_for_model_series.py
@@ -31,74 +31,68 @@ def run_tasks(config_path, cuda_devices):
 
 
 def run_glue_for_model_series_fast(config, work_dir):
-    tasks = []
-    path_exists = os.path.exists(config.model_series_dir)
-    if "fairlib" in str(config.model_series_dir) and "ensemble" not in str(
-        config.model_series_dir
+    tasks = []    
+    base_model = config.base_model_name
+    model_type = config.model_type
+    ue_reg = config.ue_reg
+    task_name = config.task_name
+    if task_name == 'asap':
+        prompt_id = config.pormpt_id
+        model_series_dir = f'/content/drive/MyDrive/workdir/trained_models/{base_model}/{model_type}_{ue_reg}/asap/prompt_id_{prompt_id}'
+
+    elif task_name == 'riken':
+        question_id = config.question_id
+        prompt_id = config.prompt_id
+        score_id = config.score_id
+        model_series_dir = f'/content/drive/MyDrive/workdir/trained_models/{base_model}/{model_type}_{ue_reg}/riken/{question_id}_{prompt_id}_{score_id}'
+    path_exists = os.path.exists(model_series_dir)
+    if "fairlib" in str(model_series_dir) and "ensemble" not in str(
+        model_series_dir
     ):
-        listdir = [f"{config.model_series_dir}_{seed}" for seed in config.seeds]
+        listdir = [f"{model_series_dir}_{seed}" for seed in config.seeds]
     elif path_exists:
-        listdir = os.listdir(config.model_series_dir)
+        listdir = os.listdir(model_series_dir)
     else:
         exit()
         print(config.seeds)
         listdir = [int(seed) for seed in config.seeds]
     for model_dir_name in listdir:
-        if "fairlib" in str(config.model_series_dir) and "ensemble" not in str(
-            config.model_series_dir
+        if "fairlib" in str(model_series_dir) and "ensemble" not in str(
+            model_series_dir
         ):
             model_path = Path(model_dir_name) / "models"
         elif path_exists:
-            model_path = Path(config.model_series_dir) / model_dir_name
-            if "fairlib" in str(config.model_series_dir):
+            model_path = Path(model_series_dir) / model_dir_name + '/id0'
+            if "fairlib" in str(model_series_dir):
                 # ensemble & fairlib case
                 model_path = Path(model_path) / "models"
             if config.script == 'run_gp.py':
                 model_path = Path(model_path) / "model"
         else:
-            model_path = config.model_series_dir
+            model_path = model_series_dir
 
         model_args_str = config.args
         model_args_str += " "
         model_args_str += f"model.model_name_or_path={model_path}"
 
-        if "fairlib" in str(config.model_series_dir) and "ensemble" not in str(
-            config.model_series_dir
-        ):
-            seed = model_dir_name.split("_")[-1]
-        elif "ensemble" in str(config.output_dir):
-            seed = config.output_dir.split("/")[-1]
-        else:
-            seed = str(model_dir_name)
+        seed = str(model_dir_name)
 
         args_str = model_args_str
         args_str += " "
-        if "fairlib" in str(config.model_series_dir) and "ensemble" not in str(
-            config.model_series_dir
-        ):
-            output_dir = str(Path(work_dir) / "results" / str(seed))
-        else:
-            output_dir = str(Path(work_dir) / "results" / str(model_dir_name))
+        output_dir = str(Path(work_dir) / "results" / f"fold_{model_series_dir[-1]}")
         args_str += f"hydra.run.dir={output_dir}"
         args_str += " "
         args_str += f"output_dir={output_dir}"
         args_str += " "
         args_str += "do_train=False do_eval=True"
         if 'asap' in config.config_path:
-            model_type = config.model_series_dir.split('/')[-3].split('_')[0]
             args_str += " "
-            args_str += f"data.prompt_id={config.model_series_dir[-1]}"
+            args_str += f"data.prompt_id={prompt_id}"
             args_str += " "
             args_str += f"data.fold={model_dir_name[-1]}"
             args_str += " "
             args_str += f"model.model_type={model_type}"
         elif 'riken' in config.config_path:
-            print(model_dir_name)
-            model_type = config.model_series_dir.split('/')[-3].split('_')[0]
-            riken_ids = config.model_series_dir.split('/')[-1].split('_')
-            question_id = riken_ids[0]
-            prompt_id = riken_ids[1]+'_'+riken_ids[2]+'_'+riken_ids[3]
-            score_id = '_'.join(riken_ids[4:])
             args_str += " "
             args_str += f"data.question_id={question_id}"
             args_str += " "
@@ -126,7 +120,7 @@ def run_glue_for_model_series_fast(config, work_dir):
             "environ": "",
             "command": config.script,
             "name": f"model_{model_dir_name}"
-            if "fairlib" not in str(config.model_series_dir)
+            if "fairlib" not in str(model_series_dir)
             else f"model_fairlib_{seed}",
             "args": args_str,
         }
